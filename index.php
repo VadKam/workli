836c72b5d437f247319159ec48b72fd95bfd38d5
VadKam <W6IDOhm>
2014-12-09 15:39:46 +0500

    начало конца

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vad Kaminskiy
+ * Date: 09.12.2014
+ * Time: 15:57
+ */
+header("Content-Type: text/html; charset=utf-8");
+
+require_once __DIR__ . '/config.php';
+include __DIR__ . 'views/allnews.php';
---
+++ views/cssjs.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vad Kaminskiy
+ * Date: 09.12.2014
+ * Time: 16:33
+ */ 
